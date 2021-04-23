a29b3a44323178cdaaf862752912c38e310fe5cd
PegJac <peggy.jacobson@outlook.com>
2021-04-23 13:36:18 +0200

    task 3 - buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React from 'react';
 import './App.scss';
 import ComponentA from './components/componentA';
 import ComponentB from './components/componentB';
 
-function App() {
-  return (
-    <div className="App">
-      <ComponentA />
-      <ComponentB />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      active: false
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    return (
+      this.state.active ? this.setState({ active: false }) : this.setState({ active: true })
+    )
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <ComponentA active={this.state.active} handleClick={this.handleClick} />
+        <ComponentB active={this.state.active} handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/componentA/index.js
+++ src/components/componentA/index.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class ComponentA extends Component {
     render() {
         return (
-            <div className={"component"}>
+            < div className={`component ${this.props.active ? "blue" : "pink"}`
+            }>
                 <span className={"component_title"}> Component A </span>
-            </div>
+                <button onClick={this.props.handleClick}> Click me </button>
+            </div >
         )
     }
 }
